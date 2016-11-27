a56e34dd159cf02346284dd3c4f93a4bdb8a8efe
Pedro Mancheno <pedromancheno@gmail.com>
2016-11-27 20:10:20 +0100

    Initial commit

---
+++ PMTaggedString/PMTaggedString/PMTaggedString.h
@@ -0,0 +1,19 @@
+//
+//  PMTaggedString.h
+//  PMTaggedString
+//
+//  Created by Pedro Mancheno on 11/27/16.
+//  Copyright © 2016 Pedro Mancheno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PMTaggedString.
+FOUNDATION_EXPORT double PMTaggedStringVersionNumber;
+
+//! Project version string for PMTaggedString.
+FOUNDATION_EXPORT const unsigned char PMTaggedStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMTaggedString/PublicHeader.h>
+
+
